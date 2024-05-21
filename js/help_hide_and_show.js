0f1214bf529744af6d73182675361eb230a652da
krejcir <krejcir@ics.muni.cz>
2024-05-21 07:56:42 +0200

    Add new help for sources, redesign of help.

--- js/help_hide_and_show.js
+++ js/help_hide_and_show.js
@@ -1,15 +1,33 @@
-const openHint = document.getElementById("openhint");
-const closeHint = document.getElementById("closehint");
-const helpTextDiv = document.getElementById("help_text");
+const openAbbrev = document.getElementById("open_abbrev");
+const closeAbbrev = document.getElementById("close_abbrev");
+const abbrevText = document.getElementById("abbrev_text");
 
-openHint.addEventListener("click", () => {
-  helpTextDiv.style.display = "block";
-  openHint.style.display = "none";
-  closeHint.style.display = "block";
+
+openAbbrev.addEventListener("click", () => {
+  abbrevText.style.display = "block";
+  openAbbrev.style.display = "none";
+  closeAbbrev.style.display = "block";
 });
 
-closeHint.addEventListener("click", () => {
-  helpTextDiv.style.display = "none";
-  openHint.style.display = "block";
-  closeHint.style.display = "none";
+closeAbbrev.addEventListener("click", () => {
+  abbrevText.style.display = "none";
+  openAbbrev.style.display = "block";
+  closeAbbrev.style.display = "none";
 });
+
+const openSource = document.getElementById("open_source");
+const closeSource = document.getElementById("close_source");
+const sourceText = document.getElementById("source_text");
+
+openSource.addEventListener("click", () => {
+  sourceText.style.display = "block";
+  openSource.style.display = "none";
+  closeSource.style.display = "block";
+});
+
+closeSource.addEventListener("click", () => {
+  sourceText.style.display = "none";
+  openSource.style.display = "block";
+  closeSource.style.display = "none";
+});
+
